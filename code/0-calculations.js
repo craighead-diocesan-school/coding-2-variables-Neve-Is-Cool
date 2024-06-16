cbfc0d9879b72d1f79f0d61e1c9ff473d889b507
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-06-17 09:44:37 +1200

    calculations
    done

--- code/0-calculations.js
+++ code/0-calculations.js
@@ -2,4 +2,24 @@
 // #### ----- Calculations ----- ######
 // ####################################
 
-function calculations() {}
+function calculations() {
+
+
+   // let firstNumber = 20
+   // let secondNumber = 5
+
+   // let result = firstNumber * secondNumber
+
+   // alert (result)
+
+
+   // firstNumber = prompt ('Enter a number')
+ //   secondNumber = 2
+
+  //  result = firstNumber * secondNumber
+
+ //   alert (firstNumber + ' x ' + secondNumber + ' = ' + result)
+
+
+
+}
