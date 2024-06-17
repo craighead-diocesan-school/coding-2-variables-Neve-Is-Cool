5a496d598af24a0733c0dd9628ca97ae0959b03a
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-06-18 10:06:37 +1200

    pupper age done

--- code/1-pupper-age.js
+++ code/1-pupper-age.js
@@ -6,4 +6,10 @@ function pupperAge() {
   alert('Dog years are human years times seven.')
 
   let humanYears = prompt('How old is your dog in human years?')
+  let result = humanYears * 7
+
+  alert ('Your dog is ' + result + ' years old')
+
+
+
 }
