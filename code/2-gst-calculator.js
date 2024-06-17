18c31ea6d5087f90110ce9b6edabf5048879e703
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-06-18 10:22:50 +1200

    gst

--- code/2-gst-calculator.js
+++ code/2-gst-calculator.js
@@ -3,5 +3,11 @@
 // ####################################
 
 function gstCalculator() {
-  alert('The price with GST the price and 15%.')
+  alert('The price with GST is the price and 15%.')
+
+  let cost = prompt ('What is the cost discluding GST?')
+  let result = cost * 1.15
+
+  alert ('The cost including GST is $' + result)
+
 }
