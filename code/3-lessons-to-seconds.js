db0545ec63b56c62c1f58bdc5527e195fae9986f
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-06-18 10:31:30 +1200

    life remaining, lessons to seconds done.

--- code/3-lessons-to-seconds.js
+++ code/3-lessons-to-seconds.js
@@ -4,4 +4,11 @@
 
 function lessonsToSeconds() {
   alert('There are 60 minutes in a lesson, and 60 seconds in a minute.')
+
+  let lessons = prompt ('How many lessons do you have left today?')
+
+  let result = lessons *60 *60
+
+  alert ("That's " + result + ' seconds 🤣🤣🤣 ')
+
 }
--- code/4-test-life-remaining.js
+++ code/4-test-life-remaining.js
@@ -3,5 +3,10 @@
 // ####################################
 
 function lifeRemaining() {
+
+    let age = prompt ('How old are you?')
   
+    let result = 83.5 - age
+
+    alert ("You've got " + result + ' years left in you 🤞')
 }
