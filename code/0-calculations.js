e57386755380a81dd6c8b8ae8b7bf5195db015dd
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-07-29 09:29:10 +1200

    Expected Testing
    Tested function with expected unit of 5

--- code/0-calculations.js
+++ code/0-calculations.js
@@ -4,22 +4,20 @@
 
 function calculations() {
 
+  alert (8+2)
+  alert (8-2)
+  alert (8*2)
+  alert (10/4)
 
-   // let firstNumber = 20
-   // let secondNumber = 5
+let firstNumber = 20
+let secondNumber = 5
+let result = firstNumber * secondNumber
+alert (result)
 
-   // let result = firstNumber * secondNumber
-
-   // alert (result)
-
-
-   // firstNumber = prompt ('Enter a number')
- //   secondNumber = 2
-
-  //  result = firstNumber * secondNumber
-
- //   alert (firstNumber + ' x ' + secondNumber + ' = ' + result)
-
+firstNumber = prompt ('Enter a number')
+secondNumber = 2
+result = firstNumber * secondNumber
+alert (firstNumber + ' x ' + secondNumber + ' = ' + result)
 
 
 }
